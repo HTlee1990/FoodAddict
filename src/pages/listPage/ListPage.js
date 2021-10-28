472e54a1d2d67bd5e616f958e8673c3fe55791e5
Mark <notbibu@gmail.com>
2021-10-28 18:58:21 +0900

    [Client]Fix: searchBtn

--- src/pages/listPage/ListPage.js
+++ src/pages/listPage/ListPage.js
@@ -1,7 +1,24 @@
+import { useState } from "react";
+import axios from "axios";
 import "./ListPage.scss";
 import { IoSearch } from "react-icons/io5";
 
+const ENDPOINT = process.env.REACT_APP_ENDPOINT;
+
 const ListPage = () => {
+  const [enteredText, setEnteredText] = useState("");
+
+  const inputHandler = (e) => {
+    setEnteredText(e.target.value);
+  };
+  const searchHandler = async () => {
+    const res = await axios.post(
+      `${ENDPOINT}/search`,
+      { data: enteredText },
+      { withCredentials: true }
+    );
+    console.log(res);
+  };
   return (
     <div className="list__header">
       <div className="categories__container">
@@ -15,8 +32,13 @@ const ListPage = () => {
             className="search__input"
             placeholder="enter your search"
             aria-label="search"
+            onChange={inputHandler}
           />
-          <button className="search__bar__btn" aria-label="search button">
+          <button
+            className="search__bar__btn"
+            aria-label="search button"
+            onClick={searchHandler}
+          >
             <IoSearch />
           </button>
         </div>
